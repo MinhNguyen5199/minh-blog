257d3a0ced655c20b4104a30a8ca5b5527f0b6ea
MinhNguyen5199 <hoangminhabc11@gmail.com>
2024-01-06 17:02:58 -0700

    blog website

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import styles from './not-found.module.css';
+
+export default function NotFound() {
+  return (
+    <div className={styles.container}>
+      <dl>
+        <dt className={styles.title}>404</dt>
+        <dd className={styles.text}>Page not found</dd>
+      </dl>
+    </div>
+  );
+}
---
+++ components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.cr}>© Minh&rsquo;s Blog. All Rights Reserved </p>
+    </footer>
+  );
+}
